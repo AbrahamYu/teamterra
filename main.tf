1ea648101b03fdd409982a6353d50b8ef0731fca
AbrahamYu <cpttkrjin@gmail.com>
2022-12-29 12:53:19 +0900

    first up

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      # 2.x 버전의 AWS 공급자 허용
-      version = "~> 2.0"
-    }
-  }
-}
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
@@ -29,6 +21,11 @@ resource "aws_internet_gateway" "VPC_GeC_IDC_IGW" {
   }
 }
 
+# resource "aws_internet_gateway_attachment" "igw_attach" {
+#   vpc_id              = aws_vpc.VPC_GeC_IDC.id
+#   internet_gateway_id = aws_internet_gateway.VPC_GeC_IDC_IGW
+# }
+
 # 라우트 테이블
 resource "aws_route_table" "VPC_GeC_IDC_RT" {
   vpc_id = aws_vpc.VPC_GeC_IDC.id
@@ -39,4 +36,169 @@ resource "aws_route_table" "VPC_GeC_IDC_RT" {
   tags = {
     Name = "VPC_${var.tag_name}_RT"
   }
+}
+
+#
+resource "aws_route" "IDCRout_1" {
+  route_table_id         = aws_route_table.VPC_GeC_IDC_RT.id
+  destination_cidr_block = var.IDCRout_1
+  gateway_id             = aws_internet_gateway.VPC_GeC_IDC_IGW.id
+  depends_on             = [var.rt_id]
+}
+
+# cgw route
+resource "aws_route" "IDCRout_2" {
+  route_table_id         = aws_route_table.VPC_GeC_IDC_RT.id
+  destination_cidr_block = var.IDCRout_2
+  instance_id            = aws_instance.IDC_Gec_CGW.id
+  depends_on             = [var.instance_cgw_id]
+}
+
+# 라우트 테이블 서브넷 연결
+resource "aws_route_table_association" "IDCSubnetRTAssociation" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.VPC_GeC_IDC_RT.id
+}
+
+# 서브넷
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.VPC_GeC_IDC.id
+  cidr_block              = var.subnet
+  availability_zone       = var.subnet_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.tag_name}_subnet"
+  }
+}
+
+# 인스턴스
+# Create a instance cgw
+resource "aws_instance" "IDC_Gec_CGW" {
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = var.instance_key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data              = file("${var.instance_file_cgw}")
+  tags = {
+    Name = "${var.instance_name}-CGW"
+  }
+}
+
+resource "aws_instance" "IDC_Gec_DB" {
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = var.instance_key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data              = file("${var.instance_file_db}")
+  tags = {
+    Name = "${var.instance_name}-DB"
+  }
+}
+
+resource "aws_instance" "IDC_Gec_DNS" {
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = var.instance_key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data              = file("${var.instance_file_dns}")
+  tags = {
+    Name = "${var.instance_name}-DNS"
+  }
+}
+
+#보안그룹
+resource "aws_security_group" "sg" {
+  description = "aws_security_group"
+  name        = var.security_group_name
+  vpc_id      = aws_vpc.VPC_GeC_IDC.id
+}
+
+# 보안그룹 룰s
+resource "aws_security_group_rule" "allow_sever_http_inbound" {
+  type              = var.security_group_rule_type
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = "8080"
+  to_port     = "8080"
+  protocol    = var.sg_protocol_tcp
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+resource "aws_security_group_rule" "allow_sever_ssh_inbound" {
+  type              = var.security_group_rule_type
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = "22"
+  to_port     = "22"
+  protocol    = var.sg_protocol_tcp
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+resource "aws_security_group_rule" "allow_sever_icmp_inbound" {
+  type              = var.security_group_rule_type
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = "-1"
+  to_port     = "-1"
+  protocol    = var.sg_protocol_tcp
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+resource "aws_security_group_rule" "allow_sever_dns_tcp_inbound" {
+  type              = var.security_group_rule_type
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = "53"
+  to_port     = "53"
+  protocol    = var.sg_protocol_tcp
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+resource "aws_security_group_rule" "allow_sever_dns_udp_inbound" {
+  type              = var.security_group_rule_type
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = "53"
+  to_port     = "53"
+  protocol    = var.sg_protocol_udp
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+resource "aws_security_group_rule" "allow_sever_https_inbound" {
+  type              = var.security_group_rule_type
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = "443"
+  to_port     = "443"
+  protocol    = var.sg_protocol_tcp
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+resource "aws_security_group_rule" "allow_sever_outbound" {
+  type              = var.security_group_rule_type_1
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [var.sg_cidr_blocks]
+}
+
+
+# eni
+resource "aws_network_interface" "test" {
+  subnet_id         = aws_subnet.subnet.id
+  private_ips       = [var.private_ips]
+  security_groups   = [aws_security_group.sg.id]
+  source_dest_check = false
+
+  attachment {
+    instance     = aws_instance.IDC_Gec_CGW.id
+    device_index = 1
+  }
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.IDC_Gec_CGW.id
+  vpc      = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.IDC_Gec_CGW.id
+  allocation_id = aws_eip.lb.id
 }
--- variabled.tf
+++ variabled.tf
@@ -13,7 +13,100 @@ variable "tag_name" {
   type        = string
 }
 
+variable "rt_id" {
+  description = "rt_cidr"
+  type        = string
+}
+
 variable "rt_cidr" {
   description = "rt_cidr"
+  type        = string
+}
+
+variable "subnet" {
+  description = "subnet"
+  type        = string
+}
+
+variable "subnet_zone" {
+  description = "subnet_zone"
+  type        = string
+}
+
+variable "IDCRout_1" {
+  description = "subnet_zone"
+  type        = string
+}
+
+variable "IDCRout_2" {
+  description = "IDCRout_2"
+  type        = string
+}
+# 보안그룹
+variable "security_group_name" {
+  description = "security_group_name"
+  type        = string
+}
+
+variable "security_group_rule_type" {
+  description = "security_group_name"
+  type        = string
+}
+variable "security_group_rule_type_1" {
+  description = "security_group_rule_type_1"
+  type        = string
+}
+
+variable "sg_cidr_blocks" {
+  description = "cidr_blocks"
+  type        = string
+}
+
+variable "sg_protocol_udp" {
+  description = "cidr_blocks"
+  type        = string
+}
+variable "sg_protocol_tcp" {
+  description = "cidr_blocks"
+  type        = string
+}
+
+# 인스턴스
+variable "instance_cgw_id" {
+  description = "instance_name"
+  type        = string
+}
+variable "instance_name" {
+  description = "instance_name"
+  type        = string
+}
+variable "instance_ami" {
+  description = "instance_ami"
+  type        = string
+}
+variable "instance_type" {
+  description = "instance_type"
+  type        = string
+}
+variable "instance_key_name" {
+  description = "instance_key_name"
+  type        = string
+}
+variable "instance_file_cgw" {
+  description = "instance_file_cgw"
+  type        = string
+}
+variable "instance_file_db" {
+  description = "instance_file_db"
+  type        = string
+}
+variable "instance_file_dns" {
+  description = "instance_file_dns"
+  type        = string
+}
+
+# eni
+variable "private_ips" {
+  description = "instance_file_dns"
   type        = string
 }
